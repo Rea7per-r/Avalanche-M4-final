5f8582d91efd1b3d0fec7492d936cf36e5d56452
Rea7per-r <118007761+Rea7per-r@users.noreply.github.com>
2023-05-23 21:32:11 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.8.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.8.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.8.3/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
-
-    constructor() ERC20("Degen", "DGN") {}
-
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+    string public storeitems;
+    constructor() ERC20("degen", "DG") {
+        storeitems="Available items are: 1.Degen oversized-T, 2.Degen cargos, 3.Degen shirts, 4.Degen sneakers, 5.Degen Hoodie";
     }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    function redeemtoken(uint256 amount) public {
+        require(amount<=5,"oops there's only 5 items in the shop");
+        _burn(msg.sender,amount*1000);
+    }
+
 }
