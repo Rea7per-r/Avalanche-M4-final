696bf43eb3fd14427dd226765d297286d6713762
Rea7per-r <118007761+Rea7per-r@users.noreply.github.com>
2023-05-28 17:04:25 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,22 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts@4.8.3/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.8.3/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts@4.8.3/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, ERC20Burnable, Ownable {
-    string public storeitems;
+    struct storeitems {
+        string name;
+        uint256 price;
+    }
+
+    mapping(uint256 => storeitems) public store;
+
     constructor() ERC20("degen", "DG") {
-        storeitems="Available items are: 1.Degen oversized-T, 2.Degen cargos, 3.Degen shirts, 4.Degen sneakers, 5.Degen Hoodie";
+        store[1] = storeitems("Degen oversized-T", 2000);
+        store[2] = storeitems("Degen cargos", 4000);
+        store[3] = storeitems("Degen shirts", 100);
+        store[4] = storeitems("Degen sneakers", 10000);
+        store[5] = storeitems("Degen Hoodie", 6000);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-    function redeemtoken(uint256 amount) public {
-        require(amount<=5,"oops there's only 5 items in the shop");
-        _burn(msg.sender,amount*1000);
+
+    function redeemToken(uint256 itemId) public {
+        require(itemId >= 1 && itemId <= 5, "Invalid item ID");
+        require(balanceOf(msg.sender) >= store[itemId].price, "Insufficient balance");
+
+        _burn(msg.sender, store[itemId].price);
+        emit ItemPurchased(msg.sender, store[itemId].name);
     }
 
+    event ItemPurchased(address  buyer, string item);
 }
